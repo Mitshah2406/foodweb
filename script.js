019d0ea1c1059e8a5fb8158f74943bf1e2f7c77d
Mit Shah <mitshah2406@gmail.com>
2021-12-06 12:11:21 +0530

    first

---
+++ script.js
@@ -0,0 +1,44 @@
+let navlist = document.querySelector('.navlist');
+let hamburgerMenu = document.querySelector('#hamburgerMenu');
+hamburgerMenu.onclick = function(){
+    hamburgerMenu.classList.toggle('fa-times');
+    navlist.classList.toggle('active');
+}
+
+  var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+  
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
